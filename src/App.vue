abaf4ff617349876c825750886c8b8b1418f0055
grish2018 <grigorisargsyan2016@gmail.com>
2021-03-16 21:06:46 +0100

    'header'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
 <template>
   <div id="app">
-  
+    <Header />
+    <main></main>
   </div>
 </template>
 
 <script>
-
-
+import Header from "./components/Header";
 export default {
-  name: 'App',
-  components: {
-   
-  }
-}
+  name: "App",
+  components: { Header },
+};
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0px;
+  height: 100vh;
+}
+@font-face {
+  //Helvetica Neue is paid
+  font-family: "Helvetica Neue";
+  src: local("Helvetica Neue"),
+    url(./fonts/LiberationSans-Regular.ttf) format("truetype");
+}
 #app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+main {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background: #f6f8ff;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <span class="header__header"> Тестовое задание </span>
+    <GraphemeLogo />
+  </header>
+</template>
+
+<script>
+import GraphemeLogo from "@/assets/icons/graphemeLogo.svg";
+export default {
+  name: "Header",
+  components: {
+    GraphemeLogo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  padding: 0 24px;
+  align-items: center;
+  justify-content: space-between;
+  &__header {
+    font-family: "Helvetica Neue";
+    font-size: 34px;
+    line-height: 41px;
+    color: #000000;
+  }
+}
+</style>
